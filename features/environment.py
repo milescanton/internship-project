191fd45e8fe041f0f540d455e7fda1000184f417
Miles C <cantonmiles@gmail.com>
2026-02-18 21:09:16 -0500

    Internship Task 4 Files

--- features/environment.py
+++ features/environment.py
@@ -7,11 +7,11 @@ from app.application import Application
 def browser_init(context, scenario_name):
 
     #Chrome
-    context.driver = webdriver.Chrome()
-    context.driver.maximize_window()
-    context.driver.implicitly_wait(4)
-    context.driver.wait = WebDriverWait(context.driver, timeout=10)
-    context.app = Application(context.driver)
+    # context.driver = webdriver.Chrome()
+    # context.driver.maximize_window()
+    # context.driver.implicitly_wait(4)
+    # context.driver.wait = WebDriverWait(context.driver, timeout=10)
+    # context.app = Application(context.driver)
 
 
     #Firefox
@@ -35,6 +35,28 @@ def browser_init(context, scenario_name):
     # context.app = Application(context.driver)
 
 
+    #BROWSERSTACK
+    bs_user = ''
+    bs_key = ''
+
+    url = f'https://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
+    options = Options()
+    bstack_options = {
+        "os" : "Windows",
+        "osVersion" : "11",
+        "browserVersion" : "latest",
+        'browserName': 'Chrome',
+        'sessionName': scenario_name,
+    }
+    options.set_capability('bstack:options', bstack_options)
+    context.driver = webdriver.Remote(command_executor=url, options=options)
+
+    context.driver.maximize_window()
+    context.driver.implicitly_wait(4)
+    context.driver.wait = WebDriverWait(context.driver, timeout=10)
+    context.app = Application(context.driver)
+
+
 def before_scenario(context, scenario):
     print('\nStarted scenario: ', scenario.name)
     browser_init(context, scenario.name)
@@ -49,5 +71,25 @@ def after_step(context, step):
         print('\nStep failed: ', step)
 
 
-def after_scenario(context, feature):
-    context.driver.quit()
+def after_scenario(context, scenario):
+
+    try:
+        if scenario.status == "passed":
+            context.driver.execute_script(
+                'browserstack_executor: {"action": "setSessionStatus", '
+                '"arguments": {"status":"passed","reason": "Assertions passed"}}'
+            )
+        else:
+            context.driver.execute_script(
+                'browserstack_executor: {"action": "setSessionStatus", '
+                f'"arguments": {{"status":"failed","reason": "{scenario.name} failed"}}}}'
+            )
+
+    except Exception as e:
+        context.driver.execute_script(
+            f'browserstack_executor: {{"action": "setSessionStatus", '
+            f'"arguments": {{"status":"failed","reason": "{str(e)}"}}}}'
+        )
+
+    finally:
+        context.driver.quit()
